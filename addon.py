758bc900b8b4e7b251bb95610098b8f1aa96c05b
leopheard <leopheard.misc@gmail.com>
2019-10-21 20:34:14 -0400

    upload

--- addon.py
+++ addon.py
@@ -2,19 +2,18 @@ from xbmcswift2 import Plugin, xbmcgui
 from resources.lib import mainaddon
 
 plugin = Plugin()
-url1 = "http://thedollop.libsyn.com/"
-url2 = "https://thedollop.libsyn.com/webpage/page/1/size/10000"
+url1 = "https://rss.art19.com/the-dollop"
 @plugin.route('/')
 def main_menu():
     items = [
       {
             'label': plugin.get_string(30001), 
             'path': plugin.url_for('episodes1'),
-            'thumbnail': "https://i1.sndcdn.com/avatars-000114476892-gzl586-large.jpg"},
+            'thumbnail': "https://content.production.cdn.art19.com/images/8e/72/72/44/8e727244-6c7d-424f-bcf4-e5ff2ab5cab7/9f27657af86c0f796164eca0c1d3ed8b9ddddee8ba4105dfa37dc60c92b611753de791c55e46120e0f5a88207981c2546677e6806bcdd0e455a185e54a42e57d.jpeg"},
         {
             'label': plugin.get_string(30000), 
             'path': plugin.url_for('episodes'),
-            'thumbnail': "https://i1.sndcdn.com/avatars-000114476892-gzl586-large.jpg"},
+            'thumbnail': "https://content.production.cdn.art19.com/images/8e/72/72/44/8e727244-6c7d-424f-bcf4-e5ff2ab5cab7/9f27657af86c0f796164eca0c1d3ed8b9ddddee8ba4105dfa37dc60c92b611753de791c55e46120e0f5a88207981c2546677e6806bcdd0e455a185e54a42e57d.jpeg"},
    ]
     return items
 
@@ -26,8 +25,8 @@ def episodes1():
     return items
 @plugin.route('/episodes/')
 def episodes():
-    soup2 = mainaddon.get_soup2(url2)
-    playable_podcast = mainaddon.get_playable_podcast(soup2)
+    soup1 = mainaddon.get_soup1(url1)
+    playable_podcast = mainaddon.get_playable_podcast(soup1)
     items = mainaddon.compile_playable_podcast(playable_podcast)
     return items
 if __name__ == '__main__':
--- resources/lib/mainaddon.py
+++ resources/lib/mainaddon.py
@@ -7,58 +7,24 @@ def get_soup1(url1):
     soup1 = BeautifulSoup(page.text, 'html.parser')
     print("type: ", type(soup1))
     return soup1
-get_soup1("http://thedollop.libsyn.com/")
-def get_soup2(url2):
-    page = requests.get(url2)
-    soup2 = BeautifulSoup(page.text, 'html.parser')
-    print("type: ", type(soup2))
-    return soup2
-get_soup2("https://thedollop.libsyn.com/webpage/page/1/size/10000")
-
-def get_playable_podcast(soup2):
-    subjects = []
-    for content in soup2.find_all('td'):
-        try:        
-            link = content.find('a')
-            link = link.get('href')
-            print("\n\nLink: ", link)
-            title = content.find('a', class_="postTitle")
-            title = title.get_text()
-        except AttributeError:
-            continue
-        item = {
-                'href': link,
-                'title': title,
-                'thumbnail': "http://static1.squarespace.com/static/5cae25d58dfc8c9bbc638a9b/t/5d1237d497f79e0001145c49/1561475041378/The+Dollop+2018+logo+nameless.png",
-        }
-        subjects.append(item)
-    return subjects
-def compile_playable_podcast(playable_podcast):
-    items = []
-    for podcast in playable_podcast:
-        items.append({
-            'label': podcast['title'],
-            'thumbnail': podcast['thumbnail'],
-            'path': podcast['href'],
-            'is_playable': True,
-    })
-    return items
 
 def get_playable_podcast1(soup1):
     subjects = []
-    for content in soup1.find_all('td', limit='10'):
-        try:        
-            link = content.find('a')
-            link = link.get('href')
+    for content in soup1.find_all('item', limit=18):
+        try:
+            link = content.find('enclosure')
+            link = link.get('url')
             print("\n\nLink: ", link)
-            title = content.find('a', class_="postTitle")
+            title = content.find('title')
             title = title.get_text()
+            thumbnail = content.find('itunes:image')
+            thumbnail = thumbnail.get('href')
         except AttributeError:
             continue
         item = {
-                'href': link,
+                'url': link,
                 'title': title,
-                'thumbnail': "http://static1.squarespace.com/static/5cae25d58dfc8c9bbc638a9b/t/5d1237d497f79e0001145c49/1561475041378/The+Dollop+2018+logo+nameless.png",
+                'thumbnail': thumbnail,
         }
         subjects.append(item)
     return subjects
@@ -68,7 +34,38 @@ def compile_playable_podcast1(playable_podcast1):
         items.append({
             'label': podcast['title'],
             'thumbnail': podcast['thumbnail'],
-            'path': podcast['href'],
+            'path': podcast['url'],
+            'is_playable': True,
+    })
+    return items
+
+def get_playable_podcast(soup1):
+    subjects = []
+    for content in soup1.find_all('item'):
+        try:
+            link = content.find('enclosure')
+            link = link.get('url')
+            print("\n\nLink: ", link)
+            title = content.find('title')
+            title = title.get_text()
+            thumbnail = content.find('itunes:image')
+            thumbnail = thumbnail.get('href')
+        except AttributeError:
+            continue
+        item = {
+                'url': link,
+                'title': title,
+                'thumbnail': thumbnail,
+        }
+        subjects.append(item)
+    return subjects
+def compile_playable_podcast(playable_podcast):
+    items = []
+    for podcast in playable_podcast:
+        items.append({
+            'label': podcast['title'],
+            'thumbnail': podcast['thumbnail'],
+            'path': podcast['url'],
             'is_playable': True,
     })
     return items
