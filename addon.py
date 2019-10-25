e6a03966683981215ce41924806558adfae5e187
leopheard <leopheard.misc@gmail.com>
2019-10-25 16:04:11 -0400

    upload

--- addon.py
+++ addon.py
@@ -3,6 +3,7 @@ from resources.lib import mainaddon
 
 plugin = Plugin()
 url1 = "https://rss.art19.com/the-dollop"
+url2 = "https://dollopengland.libsyn.com/rss"
 @plugin.route('/')
 def main_menu():
     items = [
@@ -14,6 +15,10 @@ def main_menu():
             'label': plugin.get_string(30000), 
             'path': plugin.url_for('episodes'),
             'thumbnail': "https://content.production.cdn.art19.com/images/8e/72/72/44/8e727244-6c7d-424f-bcf4-e5ff2ab5cab7/9f27657af86c0f796164eca0c1d3ed8b9ddddee8ba4105dfa37dc60c92b611753de791c55e46120e0f5a88207981c2546677e6806bcdd0e455a185e54a42e57d.jpeg"},
+        {
+            'label': plugin.get_string(30002), 
+            'path': plugin.url_for('episodes2'),
+            'thumbnail': "https://is1-ssl.mzstatic.com/image/thumb/Podcasts113/v4/11/58/64/1158643e-230b-e9b2-955d-387fc1422d2e/mza_5441154048432550917.jpg/600x600bb.jpg"},
    ]
     return items
 
@@ -28,6 +33,12 @@ def episodes():
     soup1 = mainaddon.get_soup1(url1)
     playable_podcast = mainaddon.get_playable_podcast(soup1)
     items = mainaddon.compile_playable_podcast(playable_podcast)
+    return items
+@plugin.route('/episodes2/')
+def episodes2():
+    soup2 = mainaddon.get_soup2(url2)
+    playable_podcast2 = mainaddon.get_playable_podcast2(soup2)
+    items = mainaddon.compile_playable_podcast2(playable_podcast2)
     return items
 if __name__ == '__main__':
     plugin.run()
--- resources/lib/mainaddon.py
+++ resources/lib/mainaddon.py
@@ -7,6 +7,11 @@ def get_soup1(url1):
     soup1 = BeautifulSoup(page.text, 'html.parser')
     print("type: ", type(soup1))
     return soup1
+def get_soup2(url2):
+    page = requests.get(url2)
+    soup2 = BeautifulSoup(page.text, 'html.parser')
+    print("type: ", type(soup2))
+    return soup2
 
 def get_playable_podcast1(soup1):
     subjects = []
@@ -62,6 +67,37 @@ def get_playable_podcast(soup1):
 def compile_playable_podcast(playable_podcast):
     items = []
     for podcast in playable_podcast:
+        items.append({
+            'label': podcast['title'],
+            'thumbnail': podcast['thumbnail'],
+            'path': podcast['url'],
+            'is_playable': True,
+    })
+    return items
+
+def get_playable_podcast2(soup2):
+    subjects = []
+    for content in soup2.find_all('item'):
+        try:
+            link = content.find('enclosure')
+            link = link.get('url')
+            print("\n\nLink: ", link)
+            title = content.find('title')
+            title = title.get_text()
+            thumbnail = content.find('itunes:image')
+            thumbnail = thumbnail.get('href')
+        except AttributeError:
+            continue
+        item = {
+                'url': link,
+                'title': title,
+                'thumbnail': thumbnail,
+        }
+        subjects.append(item)
+    return subjects
+def compile_playable_podcast2(playable_podcast2):
+    items = []
+    for podcast in playable_podcast2:
         items.append({
             'label': podcast['title'],
             'thumbnail': podcast['thumbnail'],
